a1fb4eee549be221c093a5c3d446157608a44575
Anrat-dev <anurati.bhosekar@gmail.com>
2023-05-16 12:46:34 -0400

    registration view updated to include email field

---
+++ register/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    # phone_no = forms.CharField(max_length=10)
+    # gender = forms.ChoiceField(choices= (('M','Male'),('F','Female')('Other','Other')))
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
