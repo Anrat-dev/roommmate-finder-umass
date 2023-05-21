72eec9b4a09c568ad7749972baab76ae0d97d412
Noah Dixon <114971241+noahdixon@users.noreply.github.com>
2023-05-20 22:10:44 -0400

    Update profile page controller in views.py

--- webappl/models.py
+++ webappl/models.py
@@ -196,7 +196,7 @@ class Profile(models.Model):
         housing = models.CharField(max_length=25, choices=HOUSING, default=HOUSING[0][0])
                 
         def __str__(self):
-                return self.userid
+                return self.userid.username
 
 
 STATUS = (
--- webappl/views.py
+++ webappl/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+from webappl.models import Profile
 
 def home_page(request):
     return render(request, 'webappl/home_page.html', {})
 
 def profile_page(request):
     current_user = request.user
+    p = Profile.objects.get(userid=current_user)
+
     # print current_user.id
-    return render(request, 'webappl/profile_page.html', {'current_user':current_user})
+    return render(request, 'webappl/profile_page.html', {'current_user':current_user, 
+                                                         'gender':p.gender,
+                                                         'level_of_study':p.level_of_study,
+                                                         'year':p.year, 
+                                                         'college':p.college,
+                                                         'program':p.program,
+                                                         'sleep_habit':p.sleep_habit,
+                                                         'cleanliness':p.cleanliness,
+                                                         'social_habit':p.social_habit,
+                                                         'duration':p.duration,
+                                                         'start_season':p.start_season,
+                                                         'housing':p.housing})
