d1a9ed544af97d36ce52faba38250db9be8252e6
Dishank jhaveri <dishankjhaveri@gmail.com>
2023-05-23 06:04:03 +0530

    changes in the test suite

--- webappl/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Profile
-
-class ProfileForm(forms.ModelForm):
-
-    class Meta:
-        model = Profile
-        fields = ["phno", "profile_picture", "gender", "level_of_study", "year", "college", "program", "sleep_habit", "cleanliness", "social_habit", "duration", "start_season", "housing"]
--- webappl/tests/test_urls.py
+++ webappl/tests/test_urls.py
@@ -6,37 +6,37 @@ class URLTests(TestCase):
     def setUp(self):
         self.client = Client()
 
-    # # Unit tests
-    # def test_url_resolves_correct_view(self):
-    #     url = reverse('home_page')
-    #     self.assertEquals(resolve(url).func, views.home_page)
+    # Unit tests
+    def test_url_resolves_correct_view(self):
+        url = reverse('home_page')
+        self.assertEquals(resolve(url).func, views.home_page)
 
-    #     url = reverse('profile_page')
-    #     self.assertEquals(resolve(url).func, views.profile_page)
+        url = reverse('profile_page')
+        self.assertEquals(resolve(url).func, views.profile_page)
 
-    #     url = reverse('edit_profile_page')
-    #     self.assertEquals(resolve(url).func, views.edit_profile_page)
+        url = reverse('edit_profile_page')
+        self.assertEquals(resolve(url).func, views.edit_profile_page)
 
-    #     url = reverse('search_page')
-    #     self.assertEquals(resolve(url).func, views.search_page)
+        url = reverse('search_page')
+        self.assertEquals(resolve(url).func, views.search_page)
 
     # # Integration tests
-    # def test_url_returns_correct_html(self):
-    #     url = reverse('home_page')
-    #     response = self.client.get(url)
-    #     self.assertContains(response, '<h1>Home Page</h1>') # Assuming that your page has <h1>Home Page</h1>
+    def test_url_returns_correct_html(self):
+        url = reverse('home_page')
+        response = self.client.get(url)
+        self.assertContains(response, '<h1>Home Page</h1>') # Assuming that your page has <h1>Home Page</h1>
 
-    #     url = reverse('profile_page')
-    #     response = self.client.get(url)
-    #     self.assertContains(response, '<h1>Profile Page</h1>') # Assuming that your page has <h1>Profile Page</h1>
+        url = reverse('profile_page')
+        response = self.client.get(url)
+        self.assertContains(response, '<h1>Profile Page</h1>') # Assuming that your page has <h1>Profile Page</h1>
 
-    #     url = reverse('edit_profile_page')
-    #     response = self.client.get(url)
-    #     self.assertContains(response, '<h1>Edit Profile Page</h1>') # Assuming that your page has <h1>Edit Profile Page</h1>
+        url = reverse('edit_profile_page')
+        response = self.client.get(url)
+        self.assertContains(response, '<h1>Edit Profile Page</h1>') # Assuming that your page has <h1>Edit Profile Page</h1>
 
-    #     url = reverse('search_page')
-    #     response = self.client.get(url)
-    #     self.assertContains(response, '<h1>Search Page</h1>') # Assuming that your page has <h1>Search Page</h1>
+        url = reverse('search_page')
+        response = self.client.get(url)
+        self.assertContains(response, '<h1>Search Page</h1>') # Assuming that your page has <h1>Search Page</h1>
 
     # Functional tests
     def test_url_returns_correct_status_code(self):
